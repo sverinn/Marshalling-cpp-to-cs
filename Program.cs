1a9880c973762cf6f7f38a8f87f8f1460ca6a1bf
Dmitry Vinyar <dimavinyar@gmail.com>
2021-09-06 21:08:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Marshalling
+{
+    class Program
+    {
+        internal static class Import
+        {
+            [DllImport(@"MarshallingDLL.dll")]
+            internal static extern IntPtr getData();
+        }
+
+        // Declares a managed prototype for unmanaged function.
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public class pItem
+        {
+            public int a;
+
+            [MarshalAs(UnmanagedType.LPStr)] //we read const char* 
+            public string str;
+
+            public IntPtr items; //store a pointer (as int? no idea what's going on here)
+        }
+
+        public static pItem getData()
+        {
+            pItem testItem = new pItem { a = 0, str = "undefined", items = IntPtr.Zero }; //initialize a pItem
+            IntPtr ItemPtr = Import.getData(); //get a pointer from dll
+            testItem = Marshal.PtrToStructure<pItem>(ItemPtr); //read data located at a pointer
+            return testItem;
+        }
+
+        static void Main(string[] args)
+        {
+            pItem myItem = getData();
+            Console.WriteLine(myItem.a.ToString() + "; " + myItem.str + "; " + myItem.items.ToString());
+        }
+    }
+}
